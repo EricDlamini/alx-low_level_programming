593f440dbc09b0884812bcee00fc369fc66ecefc
EricDlamini <sdlamini.podiatrist@gmail.com>
2023-03-17 15:48:03 -0700

    praint

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - main
+ *
+ * Return: is 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\t');
+	return (0);
+}
