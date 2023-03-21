8c3d6e563bdff5270c9d9376c93bb281f17573e3
EricDlamini <sdlamini.podiatrist@gmail.com>
2023-03-21 13:22:51 -0700

    10 alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10: prints alphabets times ten
+ * 
+ * Return: always
+ */
+void print_alphabet_x10(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		char a;
+
+		for (a = 'a' ; a <= 'z' ; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		n++;
+	}
+}
