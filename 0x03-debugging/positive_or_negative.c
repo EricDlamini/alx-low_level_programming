265875a3b2d6dacfdcddb91ccf0a2dedf85e03e0
EricDlamini <sdlamini.podiatrist@gmail.com>
2023-03-23 13:34:10 -0700

    pos or neg

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * positive_or_negative - function
+ * @i: parameter to be checked
+ * Return: is 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative", i);
+	else
+		printf("%d is positive", i);
+}
