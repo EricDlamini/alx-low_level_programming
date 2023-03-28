977aff30e57812f3cd24b8451bba53436568cdeb
EricDlamini <sdlamini.podiatrist@gmail.com>
2023-03-28 13:18:04 -0700

    402 to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * reset_to_98 - changes value to 98
+ * @n: parameter to hold value
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
